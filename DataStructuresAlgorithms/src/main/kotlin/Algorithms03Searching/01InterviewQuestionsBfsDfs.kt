8b6c34ccff9810322ab09fb18f9d85c1f2cc815a
Sergio Osorio <sergio.osorio.pasaran@gmail.com>
2024-02-29 11:57:29 -0600

    interview exercise questions about sort algorithms BFS and DFS

---
+++ DataStructuresAlgorithms/src/main/kotlin/Algorithms03Searching/01InterviewQuestionsBfsDfs.kt
@@ -0,0 +1,20 @@
+package Algorithms03Searching
+
+//If you know a solution is not far from the root of the tree:
+// BFS: pass through every node level by level, useful for closer nodes
+
+//If the tree is very deep and solutions are rare:
+// BFS: DFS would take a long of time
+// DFS: use less memory
+
+//If the tree is very wide:
+// DFS: use less memory
+
+//If solutions are frequent but located deep in the tree:
+// DFS: i know where the solutions are
+
+//Determining whether a path exists between two nodes:
+// DFS: let us store any node's path
+
+//Finding the shortest path:
+// BFS: pass through all nodes level by level, so it will find the shortest path
