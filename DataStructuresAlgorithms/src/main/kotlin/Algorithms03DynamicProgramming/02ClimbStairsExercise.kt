e739607b8c52dec0bdf7b6bc055f14a3372a3915
Sergio Osorio <sergio.osorio.pasaran@gmail.com>
2024-03-01 16:27:18 -0600

    climb stairs exercise recursively more efficient using dynamic programming (cache)

--- DataStructuresAlgorithms/src/main/kotlin/Algorithms03DynamicProgramming/02ClimbStairsExercise.kt
+++ DataStructuresAlgorithms/src/main/kotlin/Algorithms03DynamicProgramming/02ClimbStairsExercise.kt
@@ -6,17 +6,23 @@ You are climbing a staircase. It takes n steps to reach the top.
 Each time you can either climb 1 or 2 steps. In how many distinct ways can you climb to the top?
  */
 
+// cache memory
+private val cache: MutableMap<Int, Int> = mutableMapOf()
 
 fun climbStairs(n: Int): Int {
+    if(cache.containsKey(n))
+        return cache[n]!!
 
     // Base case
     if (n == 0) // We achieved last stair
         return 1
     if (n < 0) { // We have done over climbing
+        cache[n] = 0
         return 0
     }
 
-    return climbStairs(n-1) + climbStairs(n-2)
+    cache[n] = climbStairs(n-1) + climbStairs(n-2)
+    return cache[n]!!
 }
 
 fun main() {
